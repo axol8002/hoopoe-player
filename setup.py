9b8856ce9a493cc1a5c82d385bf9deaac3bec992
axol8002 <adrielmolinacaceres@gmail.com>
2026-05-09 22:47:31 +0200

    Bump version from 0.1.4 to 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="hoopoe-player",
-    version="0.1.4",
+    version="0.1.5",
     description="Play any video as colorful ASCII art in your terminal",
     long_description=long_description,
     long_description_content_type="text/markdown",
